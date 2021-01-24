0361e067a179776698642bf17f258322fdb9a56a
Alimardan <53153126+AlimardanAhmadov@users.noreply.github.com>
2021-01-24 14:27:16 +0400

    Add files via upload

---
+++ main/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Registration(models.Model):
+    from_user = models.CharField(max_length=100)
+    from_email = models.EmailField(max_length=250)
+    phone_number = models.CharField(max_length=100)
+    sent = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.from_user
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField()
+    topic = models.CharField(max_length=50)
+    number = models.CharField(max_length=100)
+    message = models.TextField()    
+
+    def __str__(self):
+        return self.name
+
+
+class Career(models.Model):
+    teach_name = models.CharField(max_length=150)
+    teach_email = models.EmailField()
+    teach_number = models.CharField(max_length=200)
+    teach_files = models.FileField(upload_to='teach_files')
+    teach_message = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.teach_name
---
+++ main/views.py
@@ -0,0 +1,158 @@
+from django.shortcuts import render, HttpResponse, redirect, HttpResponseRedirect, redirect 
+from django.core.mail import send_mail, BadHeaderError
+from django.conf import settings
+from .forms import RegistrationForm, ContactForm, CareerForm
+from django.contrib.auth.decorators import login_required
+
+import stripe
+
+stripe.api_key = "sk_test_jNv3kMTJ2PwPDuJz66KBKTFx00xZiVHQLo"
+
+
+
+def home(request):
+    context = {
+        'name': 'Alimardan',
+    }
+    return render(request, "base.html", context)
+
+
+def email(request):
+    if request.method == 'POST':
+        form= RegistrationForm(request.POST or None)
+
+        if form.is_valid():
+            from_email = form.cleaned_data.get("from_email")
+            from_user = form.cleaned_data.get("from_user")
+            phone_number = form.cleaned_data.get('phone_number')
+            form.save()
+
+            comment = from_email + " istifadəçisi qeydiyyatdan keçmək üçün müraciət etdi, əlaqə nömrəsi " + phone_number
+            pro_comment = from_user
+            send_mail(pro_comment, comment, from_email, [settings.EMAIL_HOST_USER], fail_silently=False)
+
+            return redirect('home')
+    else:
+        form = RegistrationForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'base.html', context)
+
+
+def contact(request):
+    if request.method == 'POST':
+        contact_form = ContactForm(request.POST or None)
+
+        if contact_form.is_valid():
+            contact_form.save()
+            name = contact_form.cleaned_data.get("name")
+            message = contact_form.cleaned_data.get("message")
+            number = contact_form.cleaned_data.get("number")
+            email = contact_form.cleaned_data.get("email")
+            
+            topic_comment = str(name)
+            cantact_comment = email + " - " + message + " , istifadeçinin əlaqə nömrəsi: " + number
+
+            send_mail(
+                topic_comment,
+                cantact_comment,
+                email,
+                ['eelimerdan752@gmail.com'],
+                fail_silently=False
+            )
+    else:
+        contact_form = ContactForm()
+
+    context = {
+        'contact_form': contact_form,
+    }
+
+    return render(request, 'contact.html', context)
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def exams(request):
+    return render(request, 'exams.html')
+
+
+def gallery(request):
+    return render(request, 'gallery.html')    
+
+
+def career(request):
+    if request.method == "POST":
+        career_form = CareerForm(request.POST or None)
+        
+        if career_form.is_valid():
+            teach_name = career_form.cleaned_data.get('teach_name')
+            teach_message = career_form.cleaned_data.get("teach_message")
+            teach_email = career_form.cleaned_data.get("teach_email")
+            comment = teach_email + " - " + teach_message
+            user_comment = teach_name
+            career_form.save()
+            
+            
+            send_mail(
+                user_comment,
+                comment,
+                teach_email,
+                ['eelimerdan752@gmail.com'],
+                fail_silently=False,
+            )
+    else:
+        career_form = CareerForm()
+
+    context = {
+        'career_form': career_form,
+    }
+
+    return render(request, 'career.html', context)
+
+
+def abroad(request):
+    return render(request, 'abroad.html')    
+
+
+def civil_service(request):
+    return render(request, 'service.html')    
+
+
+def driving(request):
+    return render(request, 'driving.html')
+
+
+def details(request):
+    return render(request, 'details.html')
+
+
+def charge(request):
+    if request.method == 'POST':
+        print('Data: ', request.POST)
+
+        customer = stripe.Customer.create(
+            email=request.POST['email'],
+            name=request.POST['nickname'],
+            source=request.POST['stripeToken']
+        )
+
+        charge = stripe.Charge.create(
+            customer=customer,
+            amount=100,
+            currency='azn',
+            description='Purchase'
+        )
+
+        return redirect('civil_service')
+
+    return render(request, 'payment.html')
+
+
+
+def civil_service_exams(request):
+    return render(request, 'civil.html')
